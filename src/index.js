26b68c817f77e32229bc5e87c271e96adc274ada
MHaddi1 <104686607+MHaddi1@users.noreply.github.com>
2022-08-01 23:23:48 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <>
+    <h1>This Is Animal List</h1>
+    <ul>
+      <li>Lion</li>
+      <li>Tiger</li>
+      <li>Elephant</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
